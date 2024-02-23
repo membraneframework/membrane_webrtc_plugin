2290284bc353ae622511075af408390ac1d795ce
Mateusz Front <mateusz.front@swmansion.com>
2024-02-23 17:46:45 +0100

    wip

--- mix.exs
+++ mix.exs
@@ -37,6 +37,8 @@ defmodule Membrane.Template.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 1.0"},
+      {:ex_webrtc, "~> 0.1.0"},
+      {:membrane_rtp_plugin, ">= 0.0.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
