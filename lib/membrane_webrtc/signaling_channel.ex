b61459b5850c8ff8339cd146228baa78e9972c24
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-02-20 10:46:35 +0100

    Fix compilation warnings:

--- lib/membrane_webrtc/signaling_channel.ex
+++ lib/membrane_webrtc/signaling_channel.ex
@@ -2,8 +2,6 @@ defmodule Membrane.WebRTC.SignalingChannel do
   @moduledoc """
   Deprecated since v0.24.0. Use #{inspect(Membrane.WebRTC.Signaling)} instead.
   """
-  use GenServer
-
   require Logger
 
   alias Membrane.WebRTC.Signaling
@@ -14,7 +12,6 @@ defmodule Membrane.WebRTC.SignalingChannel do
   @type ex_webrtc_message :: Signaling.ex_webrtc_message()
   @type json_data_message :: Signaling.json_data_message()
 
-  @deprecated
   @spec new() :: t
   def new() do
     Logger.warning("""
@@ -24,19 +21,15 @@ defmodule Membrane.WebRTC.SignalingChannel do
     Signaling.new()
   end
 
-  @deprecated
   @spec register_peer(t, message_format: :ex_webrtc | :json_data, pid: pid) :: :ok
   defdelegate register_peer(signaling, opts \\ []), to: Signaling
 
-  @deprecated
   @spec register_element(t) :: :ok
   defdelegate register_element(signaling), to: Signaling
 
-  @deprecated
   @spec signal(t, message_content, metadata :: map) :: :ok
   defdelegate signal(signaling, message, metadata \\ %{}), to: Signaling
 
-  @deprecated
   @spec close(t) :: :ok
   defdelegate close(signaling), to: Signaling
 end
